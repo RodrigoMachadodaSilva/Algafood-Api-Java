f5079629ad471d9129e8922cfb96f3c27efb7bc8
RodrigoMachadoDaSilva <rodrigoprojetods@gmail.com>
2022-11-22 14:50:02 -0300

    Implementação dos endpoints de associação entre Usuários e Grupos

---
+++ src/main/java/com/algaworks/algafood/api/dto/model/PermissaoModel.java
@@ -0,0 +1,16 @@
+package com.algaworks.algafood.api.dto.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PermissaoModel {
+
+	private Long id;
+
+	private String Nome;
+
+	private String descricao;
+
+}
